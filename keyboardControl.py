73ef1363a9bd402a0d2174c296a750093a233a41
EnnDev <enndevelopment@gmail.com>
2024-04-07 18:23:17 -0500

    Updated Documentation

--- keyboardControl.py
+++ keyboardControl.py
@@ -1,29 +1,41 @@
-from main import tello
+from djitellopy import tello
 import keyPressModule as kp
+from time import sleep
 
 kp.init()
 drone = tello.Tello()
 drone.connect()
 print(drone.get_battery())
 
+
 def getKeyboardInput():
     lr, fb, ud, yv = 0, 0, 0, 0
     speed = 50
 
-    if kp.getKey("LEFT"): lr = -speed
-    elif kp.getKey("RIGHT"): lr = speed
+    if kp.getKey("LEFT"):
+        lr = -speed
+    elif kp.getKey("RIGHT"):
+        lr = speed
 
-    if kp.getKey("UP"): fb = speed
-    elif kp.getKey("DOWN"): fb = -speed
+    if kp.getKey("UP"):
+        fb = speed
+    elif kp.getKey("DOWN"):
+        fb = -speed
 
-    if kp.getKey("w"): ud = speed
-    elif kp.getKey("s"): ud = -speed
+    if kp.getKey("w"):
+        ud = speed
+    elif kp.getKey("s"):
+        ud = -speed
 
-    if kp.getKey("a"): yv = speed
-    elif kp.getKey("d"): yv = -speed
+    if kp.getKey("a"):
+        yv = speed
+    elif kp.getKey("d"):
+        yv = -speed
 
-    if kp.getKey("q"): drone.land()
-    if kp.getKey("e"): drone.takeoff()
+    if kp.getKey("q"):
+        drone.land()
+    elif kp.getKey("e"):
+        drone.takeoff()
 
     return [lr, fb, ud, yv]
 
